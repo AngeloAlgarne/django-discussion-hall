7bda835ca58ea11ea1a949affdd3d373b318cebc
JA Algarne <74101193+AngeloAlgarne@users.noreply.github.com>
2023-01-31 17:49:29 +0800

    added delete

--- base/models.py
+++ base/models.py
@@ -3,18 +3,24 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-'''
-Table Room with attributes: name, description, last_updated, created
-'''
-class Topic(models.Model):
-    name = models.CharField(max_length=200)
-    
-    def __str__(self):
-        return self.name
 
 '''
 Table Room with attributes: name, description, last_updated, created
 '''
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+'''
+Table Room with attributes: name, description, last_updated, created
+'''
+
+
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
@@ -23,23 +29,28 @@ class Room(models.Model):
     # participants =
     last_updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    
+
+    # class Meta has reserved words
+    class Meta:
+        # show order in descending order, or from newest to oldest
+        ordering = ['-last_updated', '-created']
+
     def __str__(self):
         return self.name
-    
+
+
 '''
 Table Discussion with attributes: user, room, body, last_updated, created
 '''
+
+
 class Discussion(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
-    
+
     last_updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.body[0:50] + '...'
-    
-    
-    
--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
+from . import forms
 
 # Create your views here.
 
@@ -23,9 +24,61 @@ def home(request):
 def room(request, pk):
     room = models.Room.objects.get(id=pk)
     discussions = models.Discussion.objects.all().filter(room=room)
-    context = {'room': room, 'discussions': discussions}
+    context = {
+        'room': room,
+        'discussions': discussions
+    }
     return render(
         request=request,
         template_name='base/room.html',
+        context=context,
+    )
+
+
+def createRoom(request):
+    form = forms.RoomForm()
+
+    if request.method == 'POST':
+        form = forms.RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(
+        request=request,
+        template_name='base/room_form.html',
+        context=context,
+    )
+
+
+def editRoom(request, pk):
+    room = models.Room.objects.get(id=pk)
+    form = forms.RoomForm(instance=room)
+    
+    if request.method == 'POST':
+        form = forms.RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(
+        request=request,
+        template_name='base/room_form.html',
+        context=context,
+    )
+
+def deleteRoom(request, pk):
+    room = models.Room.objects.get(id=pk)
+
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+
+    context = {'object': room}
+    return render(
+        request=request,
+        template_name='base/delete.html',
         context=context,
     )
